5d77a565007596260406be3d9e62ea527f403a67
LucasArusiewicz <lucasarusiewicz@gmail.com>
2017-10-25 09:55:13 -0300

    at foto.js
    at foto.js

--- cmds/foto.js
+++ cmds/foto.js
@@ -6,7 +6,7 @@ const Querystring = module.require('querystring');
 const Discord = module.require("discord.js");
 const Type = module.require('type-of-is');
 
-module.exports.run = async ((bot, message, args) => {
+module.exports.run = async((bot, message, args) => {
     if (Object.keys(args).length === 0) {
         var embed = new Discord.RichEmbed();
         embed.setColor('#' + Math.floor(Math.random() * 16777215).toString(16));
@@ -21,18 +21,18 @@ module.exports.run = async ((bot, message, args) => {
         let indice = 0;
         let numDeFotos = 1;
         let numDeFotosEnviadas = 0;
-        
+
         if ((Type.is(parseInt(args[0]), Number) && (parseInt(args[0]) > 0 && parseInt(args[0]) <= 100)) && Object.keys(args).length > 1) {
             indice = 1;
             numDeFotos = args[0];
         }
-        
+
         let link = 'https://www.google.com.br/search?q='
         var argsPesquisa = '';
         for (i = indice; i < Object.keys(args).length; i++) {
             argsPesquisa += args[i] + ' ';
         }
-        argsPesquisa = argsPesquisa.substring(0, (argsPesquisa.length - 1))
+        argsPesquisa = argsPesquisa.substring(0,(argsPesquisa.length - 1))
         var pesquisa = Querystring.stringify({ query: argsPesquisa });
         link += ('' + pesquisa).substring(6);
         link += '&source=lnms&tbm=isch';
@@ -66,10 +66,13 @@ module.exports.run = async ((bot, message, args) => {
                     texto = texto.substring(informacoes.conteudo.length);
                     urls.push(informacoes);
                 }
-                urls.forEach(function (element) {
-                    if (numDeFotosEnviadas < numDeFotos) {
-                        var s = element.conteudo.toString();
-                        s = s.replace(/\\n/g, "\\n")
+
+                for (var key in urls) {
+                    if (urls.hasOwnProperty(key)) {
+                        var element = urls[key];
+                        if (numDeFotosEnviadas < numDeFotos) {
+                            var s = element.conteudo.toString();
+                            s = s.replace(/\\n/g, "\\n")
                                 .replace(/\\'/g, "\\'")
                                 .replace(/\\"/g, '\\"')
                                 .replace(/\\&/g, "\\&")
@@ -77,22 +80,23 @@ module.exports.run = async ((bot, message, args) => {
                                 .replace(/\\t/g, "\\t")
                                 .replace(/\\b/g, "\\b")
                                 .replace(/\\f/g, "\\f");
-                        element.titulo = '' + (JSON.parse(s)).pt;
-                        element.imagem = '' + (JSON.parse(s)).ou;
-                        element.fonte = '' + (JSON.parse(s)).ru;
-                        
-                        var embed = new Discord.RichEmbed();
-                        embed.setColor('#' + Math.floor(Math.random() * 16777215).toString(16));
-                        embed.setTitle(element.titulo);
-                        embed.setURL(element.fonte)
-                        embed.setImage(element.imagem);
-                        embed.addField(`Info:`, `${element.info}`);
-                        message.channel.sendEmbed(embed);
-                        numDeFotosEnviadas++;
-                    } else {
-                        return;
+                            element.titulo = '' + (JSON.parse(s)).pt;
+                            element.imagem = '' + (JSON.parse(s)).ou;
+                            element.fonte = '' + (JSON.parse(s)).ru;
+
+                            var embed = new Discord.RichEmbed();
+                            embed.setColor('#' + Math.floor(Math.random() * 16777215).toString(16));
+                            embed.setTitle(element.titulo);
+                            embed.setURL(element.fonte);
+                            embed.setImage(element.imagem);
+                            embed.addField(`Info:`, `${element.info}`);
+                            message.channel.sendEmbed(embed);
+                            numDeFotosEnviadas++;
+                        } else {
+                            return;
+                        }
                     }
-                }, this);
+                }
             } else {
                 throw "Error: " + resp.statusCode;
             }
