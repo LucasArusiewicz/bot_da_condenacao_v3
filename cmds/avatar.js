8777da39df3af0f8b3004ef8a004dafe2c67386d
LucasArusiewicz <lucasarusiewicz@gmail.com>
2017-10-25 08:23:34 -0300

    att
    att

--- cmds/avatar.js
+++ cmds/avatar.js
@@ -1,14 +1,16 @@
-module.exports.run = async (bot, message, args) => {
-    let msg = await message.channel.send("Carregando imagem...");
+const async = require('asyncawait/async');
+const await = require('asyncawait/await');
 
-    await message.channel.send({files: [
+module.exports.run = async (function(bot, message, args) {
+    let msg = await (message.channel.send("Carregando imagem..."));
+     await (message.channel.send({files: [
         {
             attachment: message.author.displayAvatarURL,
             name: "avatar.png"
         }
-    ]});
+    ]}));
     msg.delete();
-}
+});
 module.exports.help = {
     name: "avatar"
 }
