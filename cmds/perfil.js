fcf0bf4a7317f088531850f5030798e4dadfa2f3
LucasArusiewicz <lucasarusiewicz@gmail.com>
2017-10-25 10:03:45 -0300

    at perfil.js
    at perfil.js

--- cmds/perfil.js
+++ cmds/perfil.js
@@ -1,17 +1,19 @@
+const async = require('asyncawait/async');
+const await = require('asyncawait/await');
 const Discord = module.require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async((bot, message, args) => {
     let embed = new Discord.RichEmbed()
-            .setAuthor(message.author.username)
-            .setDescription("Essas são as suas informações!")
-            .setColor('#' + Math.floor(Math.random()*16777215).toString(16))
-            .setThumbnail(message.author.avatarURL)
-            .addField("Username", `${message.author.username}#${message.author.discriminator}`)
-            .addField("ID", `${message.author.id}`)
-            .addField("Criado em", `${message.author.createdAt}`);
+        .setAuthor(message.author.username)
+        .setDescription("Essas são as suas informações!")
+        .setColor('#' + Math.floor(Math.random() * 16777215).toString(16))
+        .setThumbnail(message.author.avatarURL)
+        .addField("Username", `${message.author.username}#${message.author.discriminator}`)
+        .addField("ID", `${message.author.id}`)
+        .addField("Criado em", `${message.author.createdAt}`);
 
     message.channel.sendEmbed(embed);
-}
+});
 
 module.exports.help = {
     name: "perfil"
